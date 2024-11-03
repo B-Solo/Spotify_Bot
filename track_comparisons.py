374aca759bbe182a8536f6ed75203b4a72b8a545
B-Solo <bsolomons@yahoo.com>
2024-11-03 08:44:53 +0000

    New file for tracking comparisons

---
+++ track_comparisons.py
@@ -0,0 +1,15 @@
+from spreadsheet_playlist import SpreadsheetPlaylist, SpreadsheetTrack
+
+
+def which_is_better():
+    raise NotImplementedError
+
+
+def which_is_worse():
+    raise NotImplementedError
+
+def is_this_happy():
+    raise NotImplementedError
+
+def is_this_sad():
+    raise NotImplementedError
