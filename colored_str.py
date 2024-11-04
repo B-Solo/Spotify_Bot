2542fd6df7fc9466848462df376182472d6ded15
B-Solo <87675166+B-Solo@users.noreply.github.com>
2024-11-04 10:09:49 +0000

    Make coding standards compliant

--- colored_str.py
+++ colored_str.py
@@ -16,6 +16,15 @@ def cyan(s):
 
 
 def color_user_input(input_msg, color):
+    """Ask for user input, which will be colored when they type
+
+    Args:
+        input_msg (str): Message to send to the user prompting the input
+        color (str): A Fore color from the colorama module
+
+    Returns:
+        str: The value received from the user
+    """
     x = input(f"{input_msg}{color}")
     print(f"{Style.RESET_ALL}")
     return x
@@ -30,10 +39,18 @@ class ColoredStr():
     string: str
 
     def __init__(self, string, color=None):
-        self.string = string
+        if string:
+            self.string = string
+        else:
+            self.string = ""
         self.color = color
 
     def lower(self):
+        """Return the lower case string of the same colour
+
+        Returns:
+            ColoredStr: The current string, but in lowercase
+        """
         return ColoredStr(self.string.lower(), self.color)
 
 
@@ -41,7 +58,7 @@ class ColoredStr():
         if self.color:
             return self.color(self.string)
         return self.string
-    
+
     def __eq__(self, other):
         if isinstance(other, ColoredStr):
             return self.string == other.string
