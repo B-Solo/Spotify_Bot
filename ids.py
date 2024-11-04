9dc7aca731f2044b27bbba8d0aba149016bae66f
B-Solo <87675166+B-Solo@users.noreply.github.com>
2024-11-04 10:10:07 +0000

    Create ids.py

---
+++ ids.py
@@ -0,0 +1,11 @@
+"""
+IDs for the playlists and sheets used by this project.
+"""
+
+EVERYTHING = 'https://open.spotify.com/playlist/36d5XdCBocMKCXpFS1JoQ8?si=f0a4d20ecb764313'
+SONGS_2023 = 'https://open.spotify.com/playlist/37i9dQZF1Fa1IIVtEpGUcU?si=b409fff09912444d'
+SONGS_2022 = 'https://open.spotify.com/playlist/37i9dQZF1F0sijgNaJdgit?si=481888f3f4404b6a'
+SONGS_2021 = 'https://open.spotify.com/playlist/37i9dQZF1EUMDoJuT8yJsl?si=83094f9117684fbc'
+SONGS_2020 = 'https://open.spotify.com/playlist/37i9dQZF1EMg6nWsb4kEDa?si=1602964d65994708'
+SONGS_2019 = 'https://open.spotify.com/playlist/37i9dQZF1EthgUig02pBIz?si=943706dee97d4b82'
+MUSIC_SHEET = '1apQT3YSnxTkZEw0N3PaSpFja7uzbvWJyZ6nHj4bzpN4'
